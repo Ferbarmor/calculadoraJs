3d35c78d49db3fd1b70ec0737d91996b0169aac2
Ferbarmor <ferbarmor@gmail.com>
2025-02-03 11:27:54 +0100

    Preubas de raices

--- calculadora.js
+++ calculadora.js
@@ -6,5 +6,16 @@ function suma(a, b) {
     return a - b;
   }
   
+  function multiplicar(a,b){
+    return a*b;
+  }
+
+  function dividir(a,b){
+    return a/b;
+  }
+
+  function raiz(a){
+    return Math.sqrt(a);
+  }
   
-  module.exports = { suma, resta };//Para pasar el analizador hola
+  module.exports = { suma, resta, multiplicar,dividir,raiz };//Para pasar el analizador hola
--- calculadora.test.js
+++ calculadora.test.js
@@ -7,3 +7,14 @@ test('Suma 2 + 3 y devuelve 5', () => {
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(resta(5, 3)).toBe(2);
 });
+
+test('Multiplica 2 * 3 y devuelve 6', () => {
+    expect(multiplicar(2, 3)).toBe(6);
+});
+
+test('Divide 6 / 3 y devuelve 2', () => {
+    expect(dividir(6, 3)).toBe(2);
+});
+test('Raiz cuadrada de 4 es 2', () => {
+    expect(raiz(4)).toBe(2);
+});
