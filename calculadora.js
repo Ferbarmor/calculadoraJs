4e01f309b91aae56a866ecfcd69b6685afb5b0b1
Ferbarmor <ferbarmor@gmail.com>
2025-02-10 11:06:30 +0100

    Otra prubea

--- calculadora.js
+++ calculadora.js
@@ -1,20 +1,20 @@
 function suma(a, b) {
-  return a + b;
- }
- function resta(a, b) {
-  return a - b;
- }
- function multiplicar(a, b) {
-  return a * b;
- }
- function dividir(a, b) {
-  if (b === 0) {
-  throw new Error("No se puede dividir por cero");
+    return a + b;
   }
-  return a / b;
- }
- // Hacer las funciones accesibles en el navegador
- window.suma = suma;
- window.resta = resta;
- window.multiplicar = multiplicar;
- window.dividir = dividir;
+  
+  function resta(a, b) {
+    return a - b;
+  }
+  
+  function multiplicar(a,b){
+    return a*b;
+  }
+
+  function dividir(a,b){
+    return a/b;
+  }
+
+ 
+
+  
+  module.exports = { suma, resta, multiplicar,dividir,raiz };//Para pasar el analizador hola
